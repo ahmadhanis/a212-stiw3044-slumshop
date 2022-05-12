5cbe15bc89c891c7c1262e816b19f8dcb27a941f
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2022-05-12 19:11:32 +0800

    Create delete_product.php

---
+++ mobile/php/delete_product.php
@@ -0,0 +1,23 @@
+<?php
+if (!isset($_POST)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
+include_once("dbconnect.php");
+$prid = $_POST['prodid'];
+$sqldel = "DELETE FROM `tbl_products` WHERE product_id = '$prid'";
+if ($conn->query($sqldel) === TRUE) {
+    $response = array('status' => 'success', 'data' => null);
+    sendJsonResponse($response);
+} else {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+}
+
+function sendJsonResponse($sentArray)
+{
+    header('Content-Type: application/json');
+    echo json_encode($sentArray);
+}
+?>
